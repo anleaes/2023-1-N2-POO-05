1af1b74b29fb55b46430a8167fbd9aea02e7a856
Thiago Adam <thiagohsm2009@hotmail.com>
2023-06-18 14:58:27 -0300

    fix albums

--- apps/albums/models.py
+++ apps/albums/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Album(models.Model):
     name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
+    ano_de_lancamento = models.TextField('Ano de lancamento', max_length=30)
+    tempo_de_duracao = models.TextField('Tempo de duracao', max_length=30)
     
     class Meta:
         verbose_name = 'Album'
